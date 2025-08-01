b41529b5a63ffb0a93d57c9d7af5e0db7d10d88a
Ridoy Baidya <ridoybaidya2@gmail.com>
2025-08-01 23:18:08 +0600

    Replace default CRA placeholder with actual app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Footer from "./components/Footer";
+
+import Contact from "./pages/Contact";
+import BlogDetail from "./pages/BlogDetail";
+
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+           <Route path="/blog/:id" element={<BlogDetail />} />
+         <Route path="/contact" element={<Contact />} /> 
+        </Routes>
+      </main>
+      <Footer />
+    </Router>
   );
 }
-
 export default App;
