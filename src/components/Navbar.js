0784f05f47829429af95cca1795cceab9596e8f9
Ridoy Baidya <ridoybaidya2@gmail.com>
2025-08-02 10:20:47 +0600

    Update navbar and logo styling, add footer, and other CSS improvements

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logo from '../assets/images/logo1.png';
 
 export default function Navbar() {
     return (
         <nav className="navbar">
-            <div className="logo_bounce">
-                <span>E</span><span>x</span><span>p</span><span>l</span>
-                <span>o</span><span>r</span><span>e</span>
-                <sapn> </sapn> <span>B</span><span>a</span><span>n</span>
-                <span>g</span><span>l</span><span>a</span><span>d</span>
-                <span>e</span><span>s</span><span>h</span>
+            <div style={{ display: "flex", alignItems: "center" }}>
+                <img src={logo} alt="Explore Bangladesh Logo" className="logo-img" />
+                <div className="logo_bounce">
+                    <span>E</span><span>x</span><span>p</span><span>l</span>
+                    <span>o</span><span>r</span><span>e</span>
+                    <sapn> </sapn> <span>B</span><span>a</span><span>n</span>
+                    <span>g</span><span>l</span><span>a</span><span>d</span>
+                    <span>e</span><span>s</span><span>h</span>
+                </div>
             </div>
             <ul className="nav-links">
                 <li><Link to="/">
@@ -19,10 +23,10 @@ export default function Navbar() {
                     <span>A</span><span>b</span><span>o</span><span>u</span><span>t</span>
                 </Link></li>
                 <li><Link to="/contact">
-                 <span>C</span><span>o</span><span>n</span><span>t</span>
-                 <span>a</span><span>c</span><span>t</span>
+                    <span>C</span><span>o</span><span>n</span><span>t</span>
+                    <span>a</span><span>c</span><span>t</span>
                 </Link></li>
             </ul>
-        </nav>
+        </nav >
     );
 }
